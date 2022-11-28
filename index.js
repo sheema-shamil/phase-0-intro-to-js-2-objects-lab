e60f5c68a1fd63286f6a1eb46e0e8e429181d822
sheema <sheemashamilfarook@gmail.com>
2022-11-28 12:24:39 +0300

    compltedassingment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+const employee= {} 
+  
+ employee.name= "Franklin"; 
+ streetAdress= "Koma"; 
+  
+ function updateEmployeeWithKeyAndValue(object, key, value){ 
+     const newObj={...object}; 
+     newObj[key]= value; 
+     return newObj; 
+ } 
+ const firstEmployee = updateEmployeeWithKeyAndValue(employee, "streetAdress", "19 Koma"); 
+  
+ console.log(firstEmployee); 
+  
+ const secondEmployee=updateEmployeeWithKeyAndValue(employee['streetAddress']); 
+  
+ console.log(secondEmployee); 
